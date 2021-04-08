cff4d6cfa2e7f5d55a09c284dc20b52d21d2405b
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-08 18:55:03 +0900

    userモデルに対するバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validetes :last_name, presence: true
+  validetes :first_name, presence: true
+  validetes :last_name_pronounce, presence: true
+  validetes :first_name_pronounce, presence: true
+  validetes :birthday, presence: true
 end
