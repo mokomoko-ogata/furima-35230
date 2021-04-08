a90d1d6c359939ab9210b8ca51ba1d641dae13b6
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-08 18:57:06 +0900

    スペルミスの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validetes :last_name, presence: true
-  validetes :first_name, presence: true
-  validetes :last_name_pronounce, presence: true
-  validetes :first_name_pronounce, presence: true
-  validetes :birthday, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_pronounce, presence: true
+  validates :first_name_pronounce, presence: true
+  validates :birthday, presence: true
 end
