51c625da247983731bbc3261eda079ac2c928209
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-09 17:08:30 +0900

    validationの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  VALID_NAME_PRONOUNCE_REGEX = /\A[\p{katakana}]+\z/
   validates :nickname, presence: true
+  validates :password, presence:true, format: {with: VALID_PASSWORD_REGEX}
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_pronounce, presence: true, format: { with: /\A[\p{katakana}]+\z/}
-  validates :first_name_pronounce, presence: true, format: { with: /\A[\p{katakana}]+\z/}
+  validates :last_name_pronounce, presence: true, format: { with: VALID_NAME_PRONOUNCE_REGEX}
+  validates :first_name_pronounce, presence: true, format: { with: VALID_NAME_PRONOUNCE_REGEX}
   validates :birthday, presence: true
 end
