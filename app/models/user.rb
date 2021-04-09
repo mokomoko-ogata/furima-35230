8fc5cfc034ed94fec8522b1287a0bea533e60dc0
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-09 22:09:33 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,11 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   VALID_NAME_PRONOUNCE_REGEX = /\A[\p{katakana}]+\z/
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   validates :nickname, presence: true
-  validates :password, presence:true, format: {with: VALID_PASSWORD_REGEX}
-  validates :last_name, presence: true
-  validates :first_name, presence: true
+  validates :password, format: {with: VALID_PASSWORD_REGEX}
+  validates :last_name, presence: true, format: {with: VALID_NAME_REGEX}
+  validates :first_name, presence: true, format: {with: VALID_NAME_REGEX}
   validates :last_name_pronounce, presence: true, format: { with: VALID_NAME_PRONOUNCE_REGEX}
   validates :first_name_pronounce, presence: true, format: { with: VALID_NAME_PRONOUNCE_REGEX}
   validates :birthday, presence: true
