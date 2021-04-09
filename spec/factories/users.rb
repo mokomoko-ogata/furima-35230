a69059aeaba86650a23d63b00b653616dcbbb4cd
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-09 15:27:59 +0900

    ユーザー情報をFactoryBotで指定

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :user do
+    transient do
+      person { Gimei.name }
+    end
+    nickname {Faker::Name.initials(number: 2)}
+    email    {Faker::Internet.free_email}
+    password = Faker::Internet.password(min_length: 6)
+    password {password}
+    password_confirmation {password}
+    last_name {Faker::Name.last_name}
+    first_name {Faker::Name.first_name}
+    last_name_pronounce {person.last.katakana}
+    first_name_pronounce {person.first.katakana}
+    birthday {Faker::Date.date}
+  end
+end
