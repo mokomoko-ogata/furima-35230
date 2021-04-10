06244b93491c70d8869c540e32bf1c7eeff83b1c
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-10 14:35:23 +0900

    with_optionsを使った記述に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,14 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   VALID_NAME_PRONOUNCE_REGEX = /\A\p{katakana}+\z/
   VALID_NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-  validates :nickname, presence: true
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :last_name, presence: true, format: { with: VALID_NAME_REGEX }
-  validates :first_name, presence: true, format: { with: VALID_NAME_REGEX }
-  validates :last_name_pronounce, presence: true, format: { with: VALID_NAME_PRONOUNCE_REGEX }
-  validates :first_name_pronounce, presence: true, format: { with: VALID_NAME_PRONOUNCE_REGEX }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
+    validates :last_name, format: { with: VALID_NAME_REGEX }
+    validates :first_name, format: { with: VALID_NAME_REGEX }
+    validates :last_name_pronounce, format: { with: VALID_NAME_PRONOUNCE_REGEX }
+    validates :first_name_pronounce, format: { with: VALID_NAME_PRONOUNCE_REGEX }
+    validates :birthday
+  end
+  
 end
