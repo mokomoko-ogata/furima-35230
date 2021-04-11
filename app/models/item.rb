9f7e0fb54c60749dd4c1a7e3bf093c8f10b37979
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-11 12:39:54 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :explain
-    validates :price, format: { with: VALID_PASSWORD_REGEX }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, format: { with: VALID_PRICE_REGEX }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
   end
   with_options numericality: { other_than: 1 } do
