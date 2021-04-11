b60aa28960f98e78bfe2ba976168aac0e5aa799e
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-12 00:02:53 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,8 +12,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name, length: { maximum: 40 }
     validates :explain, length: { maximum: 1_000 }
-    validates :price, numericality: { with: /\A[0-9]+\z/ }
-    validates_numericality_of :price, { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { with: /\A[0-9]+\z/ }, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
   end
   with_options numericality: { other_than: 1 } do
