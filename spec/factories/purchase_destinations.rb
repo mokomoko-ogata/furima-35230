9579dc22d3d9fda742a84a5b89af443d89612fd6
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-14 14:23:48 +0900

    purchase_destinationの単体テスト

---
+++ spec/factories/purchase_destinations.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :purchase_destination do
+    zip_code {'123-4567'}
+    prefecture_id {2}
+    municipality { Faker::String.random }
+    address {Faker::String.random}
+    telephone_number {Faker::Number.number(digits: 11)}
+  end
+end
