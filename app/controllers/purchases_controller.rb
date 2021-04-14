eaf42243884f289eb3e4d7f2324db3f529583141
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-14 11:46:44 +0900

    商品購入ページのFormデータを保存するまでの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,27 @@
 class PurchasesController < ApplicationController
+  def index
+    @purchase_destination = PurchaseDestination.new
+    @item = Item.find_by(id:params[:item_id])
+  end
+
+  def new
+    @purchase_destination = PurchaseDestination.new
+
+  end
   
-  
+  def create
+    @item = Item.find_by(id:params[:item_id])
+    @purchase_destination = PurchaseDestination.new(purchase_params)
+    if @purchase_destination.valid?
+      @purchase_destination.save
+      redirect_to root_path  
+    else
+      render :index
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_destination).permit(:hoge, :zip_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -1,19 +1,18 @@
 class PurchaseDestination
   include ActiveModel::Model
-  attr_accessor :zip_code, :prefecture, :municipality, :address, :building_name, :telephone_number, :item_id, :user_id
+  attr_accessor :zip_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number, :item_id, :user_id, :purchase_id, :hoge
 
   with_options presence: true do
     validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :municipality
     validates :address
     validates :telephone_number, numericality: {with: /\A\d{11}+\z/}
-    validates :item_id
     validates :user_id
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Destination.create(zip_code: zip_code, prefecture: prefecture, municipality: municipality, address: address, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
+    Destination.create(zip_code: zip_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
   end
 end
