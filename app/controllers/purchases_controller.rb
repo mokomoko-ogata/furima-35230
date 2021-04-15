466218a282d54ed937d160bfb7fec4c46889477c
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-15 14:38:23 +0900

    トークンの受け取り及び決済処理

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -13,6 +13,7 @@ class PurchasesController < ApplicationController
     @item = Item.find_by(id:params[:item_id])
     @purchase_destination = PurchaseDestination.new(purchase_params)
     if @purchase_destination.valid?
+      pay_item
       @purchase_destination.save
       redirect_to root_path  
     else
@@ -22,6 +23,15 @@ class PurchasesController < ApplicationController
 
   private
   def purchase_params
-    params.require(:purchase_destination).permit(:hoge, :zip_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_destination).permit(:zip_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: purchase_params[:token],
+        currency: 'jpy'
+      )
   end
 end
--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -1,6 +1,6 @@
 class PurchaseDestination
   include ActiveModel::Model
-  attr_accessor :zip_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number, :item_id, :user_id, :purchase_id, :hoge
+  attr_accessor :zip_code, :prefecture_id, :municipality, :address, :building_name, :telephone_number, :item_id, :user_id, :purchase_id, :token
 
   with_options presence: true do
     validates :zip_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -8,6 +8,7 @@ class PurchaseDestination
     validates :address
     validates :telephone_number, numericality: { only_integer: true }, length: { in: 10..11 }
     validates :user_id
+    validates :token
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
