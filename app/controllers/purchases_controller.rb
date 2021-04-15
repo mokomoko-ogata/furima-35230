2713492444ac03772b263bbf6e247239a282785d
mokomoko-ogata <nobodyknows2405@gmail.com>
2021-04-15 23:30:57 +0900

    メソッド名の変更

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -39,7 +39,7 @@ class PurchasesController < ApplicationController
     @item = Item.find_by(id: params[:item_id])
   end
 
-  def move_to_index_for_current
+  def move_to_index
     redirect_to root_path if current_user.id == @item.user_id || @item.purchase.present?
   end
 end
